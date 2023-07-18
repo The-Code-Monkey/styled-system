30a21ed80f2d77beabc686133428021740f12f6c
Andy Wilson <andy.wilson1234@gmail.com>
2023-07-18 16:30:27 +0100

    Update types.ts

--- packages/styled-system/src/types.ts
+++ packages/styled-system/src/types.ts
@@ -53,10 +53,6 @@ interface CSSProperties
   extends StandardProperties<number | string>,
     SvgProperties<number | string> {}
 
-type CSSPropertiesWithMultiValues = {
-  [K in keyof CSSProperties]: CSSProperties[K];
-};
-
 /**
  * CSS as POJO that is compatible with CSS-in-JS libaries.
  * Copied directly from [emotion](https://github.com/emotion-js/emotion/blob/ca3ad1c1dcabf78a95b55cc2dc94cad1998a3196/packages/serialize/types/index.d.ts#L45) types
