754aa79989b46c008c9f419fc132e2e3cf55dfa4
Andy Wilson <andy.wilson1234@gmail.com>
2023-07-20 08:39:05 +0100

    test changes

--- packages/styled-system/src/types.ts
+++ packages/styled-system/src/types.ts
@@ -1,4 +1,5 @@
-import { Property, StandardProperties, SvgProperties, Pseudos } from 'csstype';
+import * as CSS from 'csstype';
+import { Property } from 'csstype';
 
 import { ObjectOrArray } from './core';
 
@@ -49,27 +50,43 @@ export type ThemeValue<K extends keyof ThemeType, ThemeType> =
   | number
   | string;
 
-interface CSSProperties
-  extends StandardProperties<number | string>,
-    SvgProperties<number | string> {}
+  export interface StyleFunction<Props extends object> {
+      (executionContext: ExecutionContext & Props): Interpolation<object>;
+  }
 
-type CSSPropertiesWithMultiValues = {
-  [K in keyof CSSProperties]: CSSProperties[K];
-};
+  export type RuleSet<Props extends object = {}> = Interpolation<Props>[];
 
-type CSSPseudosForCSSObject = { [K in Pseudos]?: CSSObject };
+  export type StyledObject<Props extends object> = Substitute<Props, CSS.Properties> & {
+      [key: string]:
+          | string
+              | number
+                  | StyleFunction<Props>
+                      | StyledObject<Props>
+                          | RuleSet<any>
+                              | undefined;
+                              };
 
-type CSSInterpolation = undefined | number | string | CSSObject;
+  export interface Keyframes {
+      id: string;
+        name: string;
+          rules: string;
+  }
 
-interface CSSOthersObjectForCSSObject {
-  [propertiesName: string]: CSSInterpolation;
-}
+export type Interpolation<Props extends object> =
+  | StyleFunction<Props>
+    | StyledObject<Props>
+      | TemplateStringsArray
+        | string
+          | number
+            | false
+              | undefined
+                | null
+                  | Keyframes
+                      | RuleSet<object>
+                        | Interpolation<Props>[];
 
 /**
  * CSS as POJO that is compatible with CSS-in-JS libaries.
  * Copied directly from [emotion](https://github.com/emotion-js/emotion/blob/ca3ad1c1dcabf78a95b55cc2dc94cad1998a3196/packages/serialize/types/index.d.ts#L45) types
  */
-export interface CSSObject
-  extends CSSPropertiesWithMultiValues,
-    CSSPseudosForCSSObject,
-    CSSOthersObjectForCSSObject {}
+export interface CSSObject extends Interpolation<{}> {}
