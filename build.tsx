83a497cc70d11ab1a12771b7998eb2164b2ad654
Andy Wilson <andy@builtbypixel.com>
2023-09-14 09:33:00 +0100

    fix: build types for styled system

--- build.tsx
+++ build.tsx
@@ -1,6 +1,10 @@
+import { exec } from 'child_process';
+
 await Bun.build({
     entrypoints: ["./packages/styled-system/src/index.ts"],
     outdir: "./packages/styled-system/dist/esm",
     target: "browser",
     format: "esm"
 })
+
+exec("cd packages/styled-system && tsc")
